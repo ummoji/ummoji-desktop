218111df8d40465b2ab75ac3956271ef091dbb31
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-29 22:50:18 -0700

    it kinda works!

---
+++ app/lib/emoji.js
@@ -0,0 +1,41 @@
+const emojiData = require('emoji-data')
+const emojiRankings = require('emoji-rankings')
+const emojilib = require('emojilib').lib
+
+var emoji = {}
+
+emoji.entries = emojiData
+  .all()
+  .map(e => {
+    var out = {
+      name: e.name,
+      short_name: e.short_name,
+      unified: e.unified,
+      char: e.render(),
+      rank: emojiRankings.find(ranked => ranked.id === e.unified).score,
+      keywords: []
+    }
+
+    // Add keywords (synonyms), if defined
+    var lib = emojilib[e.short_name]
+    if (lib) {
+      out.keywords = lib.keywords || []
+    }
+
+    return out
+  })
+  .sort((a, b) => b.rank - a.rank)
+
+emoji.matching = (query) => {
+  // make query pattern case insensitive
+  query = new RegExp(query, 'i')
+
+  return emoji.entries.filter(e => {
+    return e.short_name.match(query) ||
+    String(e.id).match(query) ||
+    e.name.match(query) ||
+    e.keywords.some(keyword => keyword.match(query))
+  })
+}
+
+module.exports = emoji
---
+++ main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron')
+let win
+
+app.on('ready', () => {
+  let opts = {
+    frame: false,
+    transparent: true,
+    width: 600
+  }
+
+  win = new BrowserWindow(opts)
+  win.loadURL(`file://${__dirname}/app/index.html`)
+})
