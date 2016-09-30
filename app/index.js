09c10e78f1aed992630652b1fac430ab2d53ff93
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-29 23:29:32 -0700

    improve yo-yo use

--- app/index.js
+++ app/index.js
@@ -1,33 +1,46 @@
 const yo = require('yo-yo')
 const emoji = require('./lib/emoji')
-var $results = yo`<ul></ul>`
-document.querySelector('results').appendChild($results)
-document.querySelector('input[name="query"]').addEventListener('input', search)
 
-function list (items) {
-  return yo`<ul>
-    ${items.map(function (item) {
-      const {name, short_name, unified, char, rank, keywords} = item
-      return yo`
-        <li
-          class="result"
-          id="${unified}"
-          data-rank="${rank}"
-          data-name="${name}"
-          data-short-name="${short_name}"
-          data-keywords=${keywords.join(', ')}
-        >
-        <div class="result-char">${char}</div>
-        <div class="result-details">
-          <span class="result-short-name">${short_name}</span>
-          <span class="result-keywords">${keywords.join(', ')}</span>
-        </div>
-        </li>`
-    })}
-  </ul>`
+var $input = yo`
+  <input
+    oninput=${search}
+    type="text"
+    name="query"
+    placeholder="Find emoji..."
+    autofocus
+    tabindex=1
+  >
+`
+var $results = yo`<ul></ul>`
+
+document.body.appendChild($input)
+document.body.appendChild($results)
+
+function renderMatch (emoji) {
+  const {name, short_name, unified, char, rank, keywords} = emoji
+  return yo`
+    <li
+      class="result"
+      id="${unified}"
+      data-rank="${rank}"
+      data-name="${name}"
+      data-short-name="${short_name}"
+      data-keywords=${keywords.join(', ')}
+    >
+    <div class="result-char">${char}</div>
+    <div class="result-details">
+      <span class="result-short-name">${short_name}</span>
+      <span class="result-keywords">${keywords.join(', ')}</span>
+    </div>
+    </li>
+  `
+}
+
+function list (matches) {
+  return yo`<ul>${matches.map(renderMatch)}</ul>`
 }
 
 function search(event) {
   var query = event.target.value
-  yo.update($results, list(emoji.matching(query).slice(0, 10)))
+  yo.update($results, list(emoji.matching(query).slice(0, 50)))
 }
