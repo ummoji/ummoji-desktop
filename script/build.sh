afcfec08b67ae71bb56d0c0eb66f3c1974143d79
Zeke Sikelianos <zeke@sikelianos.com>
2017-03-29 23:26:20 -0700

    working on that release script

--- script/build.sh
+++ script/build.sh
@@ -1,30 +1,25 @@
-nicns --in build/icon.png --out build/icon.icns
+# clean up
+mkdir -p dist
+rm -rf dist
+mkdir dist
 
+# create icons
+nicns --in build/icon.png --out build/icon.icns
 png-to-ico build/icon.png > build/icon.ico
 
-electron-zip-packager . \
-  --overwrite \
-  --platform=darwin \
-  --arch=x64 \
-  --out=dist \
-  --icon=build/icon.icns \
-  --osxSign.identity='Developer ID Application: GitHub'
+# mac
+electron-packager . --platform=darwin --arch=x64 --out=dist --icon=build/icon.icns --osxSign.identity='Developer ID Application: GitHub'
+create-dmg dist/Ummoji-darwin-x64/Ummoji.app
+mv Ummoji-$npm_package_version.dmg dist
 
-mv dist/Ummoji-darwin-x64.zip dist/Ummoji-$npm_package_version-mac.zip
+# win
+electron-packager . --platform=win32 --arch=ia32 --out=dist --icon=build/icon.ico
+cd dist/Ummoji-win32-ia32
+zip -r -X --quiet "../Ummoji-$npm_package_version-win.zip" *
+cd ../../
 
-electron-zip-packager . \
-  --overwrite \
-  --platform=win32 \
-  --arch=ia32 \
-  --out=dist \
-  --icon=build/icon.ico
-
-mv dist/Ummoji-win32-ia32.zip dist/Ummoji-$npm_package_version-win.zip
-
-electron-zip-packager . \
-  --overwrite \
-  --platform=linux \
-  --arch=x64 \
-  --out=dist
-
-mv dist/Ummoji-linux-x64.zip dist/Ummoji-$npm_package_version-linux.zip
+# linux
+electron-packager . --platform=linux --arch=x64 --out=dist
+cd dist/Ummoji-linux-x64
+zip -r -X --quiet "../Ummoji-$npm_package_version-linux.zip" *
+cd ../../
