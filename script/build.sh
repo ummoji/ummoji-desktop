034a655b4a86928020f4b695b4d7d143639f5562
Zeke Sikelianos <zeke@sikelianos.com>
2017-03-31 20:35:14 -0700

    remove versions from packaged filenames

--- script/build.sh
+++ script/build.sh
@@ -10,16 +10,16 @@ png-to-ico build/icon.png > build/icon.ico
 # mac
 electron-packager . --platform=darwin --arch=x64 --out=dist --icon=build/icon.icns --osxSign.identity='Developer ID Application: GitHub'
 create-dmg dist/Ummoji-darwin-x64/Ummoji.app
-mv Ummoji-$npm_package_version.dmg dist
+mv Ummoji-$npm_package_version.dmg dist/ummoji.dmg
 
 # win
 electron-packager . --platform=win32 --arch=ia32 --out=dist --icon=build/icon.ico
 cd dist/Ummoji-win32-ia32
-zip -r -X --quiet "../Ummoji-$npm_package_version-win.zip" *
+zip -r -X --quiet "../ummoji-win.zip" *
 cd ../../
 
 # linux
 electron-packager . --platform=linux --arch=x64 --out=dist
 cd dist/Ummoji-linux-x64
-zip -r -X --quiet "../Ummoji-$npm_package_version-linux.zip" *
+zip -r -X --quiet "../ummoji-linux.zip" *
 cd ../../
