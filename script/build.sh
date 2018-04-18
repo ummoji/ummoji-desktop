8be78bb837e6f311329bc7a4dd6d7e4e231f647a
Zeke Sikelianos <zeke@sikelianos.com>
2018-04-17 21:03:39 -0700

    whatever just keep publishing for windows and linux too

--- script/build.sh
+++ script/build.sh
@@ -19,15 +19,13 @@ electron-installer-zip dist/Ummoji-darwin-x64 dist/ummoji-mac.zip
 create-dmg dist/Ummoji-darwin-x64/Ummoji.app
 mv Ummoji-$npm_package_version.dmg dist/ummoji.dmg
 
-# TODO: renable builds for other platforms once mac auto-update is working
+electron-packager . --platform=win32 --arch=ia32 --out=dist --icon=build/icon.ico
+cd dist/Ummoji-win32-ia32
+zip -r -X --quiet "../ummoji-win.zip" *
+cd ../../
 
-# electron-packager . --platform=win32 --arch=ia32 --out=dist --icon=build/icon.ico
-# cd dist/Ummoji-win32-ia32
-# zip -r -X --quiet "../ummoji-win.zip" *
-# cd ../../
-
-# # linux
-# electron-packager . --platform=linux --arch=x64 --out=dist
-# cd dist/Ummoji-linux-x64
-# zip -r -X --quiet "../ummoji-linux.zip" *
-# cd ../../
+# linux
+electron-packager . --platform=linux --arch=x64 --out=dist
+cd dist/Ummoji-linux-x64
+zip -r -X --quiet "../ummoji-linux.zip" *
+cd ../../
